4d43afd47db4bebec92c8ea38c94a40c3b873afe
Andrew Mason <andrew@nocturnal.net.au>
2017-02-08 22:57:35 +1030

    Refactor code to make it easier to run over a dir

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
-	//"bytes"
-	//"image"
-	//"image/gif"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
 
 	"github.com/rainycape/magick"
 )
 
 func main() {
 
-	var fileName = "/home/am/projects/sdk/images/gallery/DSC_0501.JPG"
+	var dir = "/home/am/projects/sdk/images/gallery"
+
+	resizeFilesInDir(dir)
+}
+
+func resizeFilesInDir(dir string) {
+	filesInDir, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	for _, fileName := range filesInDir {
+		resizeFile(path.Join(dir, fileName.Name()))
+	}
+	os.Exit(0)
+}
+
+func resizeFile(fileName string) {
 
 	var myImage *magick.Image
 	var err error
@@ -21,57 +37,66 @@ func main() {
 	myImage, err = magick.DecodeFile(fileName)
 
 	if err != nil {
-		fmt.Println("fileName: {0}", fileName)
+		fmt.Println("fileName: {fileName}", fileName)
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	var isPortrait = IsPortrait(myImage)
 
-	var newWidth = 200
-	var newHeight = -1
+	var newWidth int
+	var newHeight int
+	var layout string
 
 	if isPortrait {
-
-		// resize to width
-		// var newImage *magick.Image
-
-		newImage, err := myImage.Resize(newWidth, newHeight, magick.FCubic)
-
-		if err != nil {
-			fmt.Printf("Could not resize %s ", fileName)
-			fmt.Println(err)
-		}
-
-		fo, err := os.Create("/tmp/output.jpg")
-		if err != nil {
-			log.Fatal(err)
-			panic(err)
-
-		}
-
-		defer fo.Close()
-		newImage.Encode(fo, magick.NewInfo())
+		newWidth = 200
+		newHeight = -1
+		layout = "portrait"
+	} else {
+		newWidth = -1
+		newHeight = 200
+		layout = "landscape"
 	}
+
+	newImage, err := myImage.Resize(newWidth, newHeight, magick.FCubic)
+
+	if err != nil {
+		fmt.Printf("Could not resize %s ", fileName)
+		fmt.Println(err)
+	}
+
+	outputFilename := path.Base(fileName)
+	newFilename := path.Join("/tmp/images", layout, outputFilename)
+
+	writeImage(newFilename, newImage)
+
+	fmt.Printf("File: %s has been resized to %d x %d \n", newFilename, newImage.Width(), newImage.Height())
+}
+
+func writeImage(newFilename string, newImage *magick.Image) {
+	fo, err := os.Create(newFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer fo.Close()
+	newImage.Encode(fo, magick.NewInfo())
 }
 
 // ShowFormats displays all the formats supported
 func ShowFormats() {
+	var formats []string
+	var err error
+	formats, err = magick.SupportedFormats()
 
-	/*
-		var formats []string
-		var err error
-		formats, err = magick.SupportedFormats()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-		if err != nil {
-			fmt.Println(err)
-		}
-	*/
-	/*
-		for _, format := range formats {
-			fmt.Println(format)
-		}
-	*/
+	for _, format := range formats {
+		fmt.Println(format)
+	}
+
 }
 
 // IsPortrait prints the current info for a given fileName
@@ -80,8 +105,8 @@ func IsPortrait(myImage *magick.Image) bool {
 	var height = myImage.Height()
 	var width = myImage.Width()
 
-	fmt.Printf("height %d \n", height)
-	fmt.Printf("width %d \n", width)
+	fmt.Printf("Current Height is: %d \n", height)
+	fmt.Printf("Current Width is: %d \n", width)
 
 	return (height > width)
 }
